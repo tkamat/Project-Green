66ffdaba20ac133de1809edc25359877e1400e5d
tkamat <platypusdiamond@gmail.com>
2016-10-14 15:09:12 -0400

    Update Calculate.java

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,22 @@
-#include <stdio.h>
-
-int main(void){
-    printf("java sucks");
-}
+public class Calculate {
+    
+    private int myCars;
+    private int myNumberofLanes;
+    private int myNumberOfTurns;
+    
+    public Calculate(int cars, int lanes, int turns) {
+        myCars = cars;
+        myNumberOfLanes = lanes;
+        myNumberOfTurns = turns;
+    }
+    
+    public int calcInstalationCost() {
+        int intersectionArea = 12.96 * myLanes;
+        int numberOfTiles = intersectionArea / 
+    }
+    
+    public int[] calcProfits() {
+        int[] profits = new int[3];
+        int installationCost = calcInstallationCost;
+        
+        
