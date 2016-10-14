a1e3d9afeac63fc6b794f32bb0a251129b680ac6
tkamat <platypusdiamond@gmail.com>
2016-10-14 15:46:33 -0400

    Update Calculate.java

--- Calculate.java
+++ Calculate.java
@@ -1,22 +1,32 @@
 public class Calculate {
     
-    private int myCars;
+    private int myCarsPerDay;
     private int myNumberofLanes;
     private int myNumberOfTurns;
     
     public Calculate(int cars, int lanes, int turns) {
-        myCars = cars;
+        myCarsPerDay = cars;
         myNumberOfLanes = lanes;
         myNumberOfTurns = turns;
     }
     
-    public int calcInstalationCost() {
-        int intersectionArea = 12.96 * myLanes;
-        int numberOfTiles = intersectionArea / 
+    public double calcInstalationCost() {
+        double tileArea = 3.216;
+        double tileCost = 122.23;
+        double intersectionArea = 12.96 * myLanes;
+        double numberOfTiles = (int)(intersectionArea / tileArea);
+        return tileCost * numberOfTiles;
     }
     
-    public int[] calcProfits() {
+    
+    public double[] calcProfits() {
         int[] profits = new int[3];
+        int energyPerTileCompress = TODO;
         int installationCost = calcInstallationCost;
-        
+        int yearlyEnergyProduction = myCarsPerDay * energyPerCompress * 
+        for (int i = 0; i < profits.length; i++) {
+            profits[i] = 
+    }
+    
+    
         
